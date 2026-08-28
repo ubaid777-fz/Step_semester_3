7d642ce989fb256e27381bcf393a1df45db39b1a
Ubaid <ubaidmohammad2007@gmail.com>
2026-08-28 15:42:16 +0530

    Add Session 1 Java code

---
+++ EmployeeInformation.java
@@ -0,0 +1,28 @@
+class Employee {
+    String empName;
+    double salary;
+    static String companyName =
+            "Bright Horizon Technologies";
+    static int employeeCount = 0;
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: "
+                + employeeCount);
+    }
+}
+public class EmployeeInformation{
+    public static void main(String[] args) {
+        Employee e1 =
+                new Employee("Divya", 65000);
+        Employee e2 =
+                new Employee("Arjun", 50000);
+        Employee e3 =
+                new Employee("Priya", 55000);
+        Employee.printCompanyInfo();
+    }
+}
---
+++ StudentDetails.java
@@ -0,0 +1,28 @@
+class Student {
+    String name;
+    int attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    Student(String name, int attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+}
+
+public class StudentDetails {
+    public static void main(String[] args) {
+
+        Student s1 = new Student("Ravi", 90);
+        Student s2 = new Student("Anitha", 85);
+
+        Student.printCollegeInfo();
+    }
+}
